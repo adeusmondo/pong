4aa60da8a2e45a42626f4f6e67be30424bf52f2e
Carlos Mondo <csvmondo@gmail.com>
2023-01-23 22:46:01 -0300

    feat: add paddles and ball, change background

--- main.lua
+++ main.lua
@@ -33,13 +33,18 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     love.graphics.printf(
-        'Hello Pong Game!',
+        'Hello to Pong Game!',
         0,
-        VIRTUAL_HEIGHT / 2- 6,
+        20,
         VIRTUAL_WIDTH,
         'center'
     )
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     push:apply('end')
 end
