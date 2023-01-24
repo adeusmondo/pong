0504b0c3b9a0d749243d8907b4987cd3a5ee6354
Carlos Mondo <csvmondo@gmail.com>
2023-01-23 22:43:27 -0300

    feat: change font to a retro font

--- main.lua
+++ main.lua
@@ -14,12 +14,15 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    retroFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(retroFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = true,
+        resizable = false,
         vsync = true
     })
-end
+end 
 
 function love.keypressed(key)
     if key == 'escape' then
